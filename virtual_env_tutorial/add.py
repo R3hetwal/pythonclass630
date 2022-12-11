a66ff58fa0a8520a375b1e9fd5f26f143fe73e34
R3hetwal <rqrakshya@gmail.com>
2022-12-11 07:42:44 +0545

    python project 630

---
+++ virtual_env_tutorial/add.py
@@ -0,0 +1,9 @@
+def add_numbers(x, y):
+    return x + y
+
+x = int(input("Enter a number x: "))
+y = int(input("Enter a number y: "))
+
+total = add_numbers(x, y)
+
+print(f'The total sum of x and y is {total}')
---
+++ virtual_env_tutorial/main.py
@@ -0,0 +1,5 @@
+from mul import multiply_numbers
+
+multiplication = multiply_numbers(8, 8)
+print(multiplication)
+
